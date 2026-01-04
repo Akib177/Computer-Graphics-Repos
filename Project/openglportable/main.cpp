aa980cfef865bccb13ad3219d53dd39384720dda
Akib177 <adnanakib244@gmail.com>
2026-01-04 22:46:37 +0600

    Draw hill

--- Project/openglportable/main.cpp
+++ Project/openglportable/main.cpp
@@ -50,3 +50,26 @@ void drawCloud(float x,float y)
     drawCircle(x-20,y+10,25);
     drawCircle(x+40,y,18);
 }
+
+//  Background
+void drawHill()
+{
+    glColor3f(0.2,0.6,0.2);
+    drawCircle(120,80,150);
+    drawCircle(360,80,180);
+}
+void drawBackground()
+{
+    glColor3f(0.53,0.81,0.98);
+    drawRect(0,0,worldW,worldH);
+
+    // Clouds
+    drawCloud(100,520);
+    drawCloud(280,560);
+    drawCloud(380,500);
+
+    drawHill();
+
+    glColor3f(0.36,0.25,0.2);
+    drawRect(0,0,worldW,80);
+}
